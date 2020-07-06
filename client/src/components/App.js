503f9f976f3194609d8302c7ae898430f3cfe9ba
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-06 16:10:02 -0600

    require exact path match

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -10,9 +10,17 @@ export default function App() {
 	return (
 		<div>
 			<BrowserRouter>
-				<div>
-					<Route path="/" component={Landing} />
-				</div>
+				<Switch>
+					<Route path="/" exact>
+						<Landing />
+					</Route>
+					<Route path="/surveys" exact>
+						<Dashboard />
+					</Route>
+					<Route path="/surveys/new" exact>
+						<SurveyNew />
+					</Route>
+				</Switch>
 			</BrowserRouter>
 		</div>
 	);
