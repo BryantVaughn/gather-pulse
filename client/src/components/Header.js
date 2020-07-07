a8c47d33640bbd31a53f60d050b1c891b5fe2459
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-06 20:45:29 -0600

    header color

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
 	render() {
 		return (
-			<nav style={{ paddingLeft: '10px', backgroundColor: '#08375b' }}>
+			<nav style={{ paddingLeft: '10px', backgroundColor: '#365c7b' }}>
 				<div className="nav-wrapper">
 					<a className="left brand-logo">GatherPulse</a>
 					<ul className="right">
