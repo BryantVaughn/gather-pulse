eea2c665e0e132283f7f85c4182d6cf7ef91ee8a
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-06 21:16:46 -0600

    refactor app component to class

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './Header';
@@ -6,25 +6,31 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-export default function App() {
-	return (
-		<div className="container">
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Switch>
-						<Route exact path="/">
-							<Landing />
-						</Route>
-						<Route exact path="/surveys">
-							<Dashboard />
-						</Route>
-						<Route exact path="/surveys/new">
-							<SurveyNew />
-						</Route>
-					</Switch>
-				</div>
-			</BrowserRouter>
-		</div>
-	);
+class App extends Component {
+	componentDidMount() {}
+
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Switch>
+							<Route exact path="/">
+								<Landing />
+							</Route>
+							<Route exact path="/surveys">
+								<Dashboard />
+							</Route>
+							<Route exact path="/surveys/new">
+								<SurveyNew />
+							</Route>
+						</Switch>
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
+
+export default App;
