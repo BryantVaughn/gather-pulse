4e9aeacf5e96b687e0f79aec1b2497f4b6cfc73e
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-08 12:34:03 -0600

    requireLogin middleware

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,9 @@
 const { stripeSecretKey } = require('../config/keys');
 const stripe = require('stripe')(stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
-	app.post('/api/payments', async (req, res) => {
+	app.post('/api/payments', requireLogin, async (req, res) => {
 		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: 'usd',
