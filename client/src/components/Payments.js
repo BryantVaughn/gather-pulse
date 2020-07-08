f317e4926343cb3d13ad0bf52482afde0adb8cdf
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-08 10:57:44 -0600

    payment button styles

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,23 @@ export default class Payments extends Component {
 	render() {
 		return (
 			<StripeCheckout
+				name="GatherPulse"
+				description="$5 for 5 survey credits"
 				amount={500}
 				token={(token) => console.log(token)}
 				stripeKey={process.env.REACT_APP_STRIPE_KEY}
-			/>
+			>
+				<button
+					className="btn"
+					style={{
+						backgroundColor: 'rgba(255,255,255,0.8)',
+						color: '#365c7b',
+						fontWeight: 'bold'
+					}}
+				>
+					Add Credits
+				</button>
+			</StripeCheckout>
 		);
 	}
 }
