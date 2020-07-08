be53245a73599a7c076157baca86b6aee2c4d8f4
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-08 12:02:42 -0600

    handle promise in charge creation

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,8 +2,8 @@ const { stripeSecretKey } = require('../config/keys');
 const stripe = require('stripe')(stripeSecretKey);
 
 module.exports = (app) => {
-	app.post('/api/payments', (req, res) => {
-		stripe.charges.create({
+	app.post('/api/payments', async (req, res) => {
+		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: 'usd',
 			description: '$5 for 5 survey credits',
