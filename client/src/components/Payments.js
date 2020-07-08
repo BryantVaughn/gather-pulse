bfdcbfdd675975fedc122174693d27af0c4b0a98
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-08 11:09:48 -0600

    add action creator to payments component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class Payments extends Component {
+class Payments extends Component {
 	render() {
 		return (
 			<StripeCheckout
 				name="GatherPulse"
 				description="$5 for 5 survey credits"
 				amount={500}
-				token={(token) => console.log(token)}
+				token={(token) => this.props.handleToken(token)}
 				stripeKey={process.env.REACT_APP_STRIPE_KEY}
 			>
 				<button
@@ -25,3 +27,5 @@ export default class Payments extends Component {
 		);
 	}
 }
+
+export default connect(null, actions)(Payments);
