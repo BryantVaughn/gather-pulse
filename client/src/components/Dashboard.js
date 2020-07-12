23c471bae4377f30b9fe251288a978402ff0bfff
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-12 16:45:05 -0600

    include link for materialize icons and add anchor to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default function Dashboard() {
-	return <div>Dashboard</div>;
+	return (
+		<div>
+			Dashboard
+			<div className="fixed-action-btn">
+				<a className="btn-floating btn-large red">
+					<i className="material-icons">add</i>
+				</a>
+			</div>
+		</div>
+	);
 }
