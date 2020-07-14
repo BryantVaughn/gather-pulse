cbfa30c45da7b74048db31cf2d5be2f24ed6bad1
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-14 13:10:27 -0600

    validatation of form input

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -50,4 +50,11 @@ class SurveyForm extends Component {
 	}
 }
 
-export default reduxForm({ form: 'surveyForm' })(SurveyForm);
+function validate(values) {
+	const errors = {};
+	if (!values.title) errors.title = 'You must provide a title';
+
+	return errors;
+}
+
+export default reduxForm({ validate, form: 'surveyForm' })(SurveyForm);
