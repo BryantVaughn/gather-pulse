6dda1d6e166afc876804852e9e53a50a1ccf5dba
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-14 15:32:44 -0600

    form validation for no value

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-export default function SurveyField({ input, label }) {
+export default function SurveyField({
+	input,
+	label,
+	meta: { error, touched }
+}) {
 	return (
 		<div>
 			<label>{label}</label>
-			<input {...input} />
+			<input style={{ marginBottom: '5px' }} {...input} />
+			<div className="red-text" style={{ marginBottom: '20px' }}>
+				{touched && error}
+			</div>
 		</div>
 	);
 }
