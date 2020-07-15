0dc91b6e94843ddb9f32d5b0b7b70c88fc55882a
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-15 11:07:19 -0600

    gather form values in review component

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function SurveyFormReview({ onCancel }) {
+function SurveyFormReview({ onCancel }) {
 	return (
 		<div>
 			<h5>Please confirm your entries</h5>
+
 			<button className="yellow darken-3 btn-flat" onClick={onCancel}>
 				Back
 			</button>
 		</div>
 	);
 }
+
+function mapStateToProps(state) {
+	return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
