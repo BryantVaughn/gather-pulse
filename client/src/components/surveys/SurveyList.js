e1cd49eadc725303ee05b098a8e16528474f95e1
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-20 15:32:23 -0600

    display survey cards on dashboard

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,8 +7,28 @@ class SurveyList extends Component {
 		this.props.fetchSurveys();
 	}
 
+	renderSurveys() {
+		return this.props.surveys.map((survey) => {
+			return (
+				<div className="card grey lighten-4" key={survey._id}>
+					<div className="card-content">
+						<span className="card-title">{survey.title}</span>
+						<p>{survey.body}</p>
+						<p className="right">
+							Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+						</p>
+					</div>
+					<div className="card-action">
+						<a>Yes: {survey.yes}</a>
+						<a>No: {survey.no}</a>
+					</div>
+				</div>
+			);
+		});
+	}
+
 	render() {
-		return <div>Survey List</div>;
+		return <div style={{ marginTop: '20px' }}>{this.renderSurveys()}</div>;
 	}
 }
 
