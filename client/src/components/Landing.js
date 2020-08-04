8c814839657da959aa2502ad56083173233827e8
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-04 12:00:11 -0600

    landing styles

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import GPLogo from '../images/GPLogo.png';
+import './styles/Landing.css';
 
 export default function Landing() {
 	return (
-		<div style={{ textAlign: 'center' }}>
-			<h1>GatherPulse</h1>
-			Collect feedback from your users!
+		<div className="root">
+			<img className="logo" src={GPLogo} />
+			<h4>Collect user feedback with GatherPulse!</h4>
+			<p>
+				GatherPulse is an easy-to-use, powerful tool that allows you to create
+				custom surveys and email them out to users. All responses are tracked
+				and displayed in your survey dashboard.
+			</p>
 		</div>
 	);
 }
